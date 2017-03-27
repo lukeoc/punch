78e1e6c4fdf2b5515ecc955a6e0038dd41587424
luke <luke.p.oconnor@gmail.com>
2017-03-26 19:09:29 -0700

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,37 @@ luke = Consumer.create(
   name: "Luke O'Connor", 
   email: "luke@luke.com", 
   password: "123", 
-  phone: "5741234567")
+  phone: "5742765882")
 
 eric = Consumer.create(
   name: "Eric Phillips", 
   email: "eric@eric.com", 
   password: "123", 
-  phone: "9871234567")
+  phone: "5742765882")
 
 james = Consumer.create(
   name: "James O'Connor",
   email: "james@james.com",
   password: "123",
-  phone: "7541234567")
+  phone: "5742765882")
 
 matt = Consumer.create(
   name: "Matt Mulvey",
   email: "matt@matt.com",
   password: "123",
-  phone: "2341234567")
+  phone: "5742765882")
 
 nate = Consumer.create(
   name: "Nate Montana",
   email: "nate@nate.com",
   password: "123",
-  phone: "4561234567")
+  phone: "5742765882")
 
 nick = Consumer.create(
   name: "Nick Montana",
   email: "nick@nick.com",
   password: "123",
-  phone: "4341234567")
+  phone: "5742765882")
 
 consumers = [luke, eric, james, matt, nate, nick]
 
@@ -46,43 +46,66 @@ bobs_donuts = Merchant.create(
   name: "Bob's Donuts", 
   email: "bobs@bobs.com", 
   reward_name: "free donut", 
-  reward_threshold: 20),
-  lat:,
-  lng:
+  reward_threshold: 20,
+  active_merchant: true,
+  lat:0,
+  lng:0)
 
 cafe_brainwash = Merchant.create(
   name: "Cafe Brainwash",
   email: "brainwash@brainwash.com",
   reward_name: "free coffee",
-  reward_threshold: 25),
-  lat:,
-  lng:
+  reward_threshold: 25,
+  active_merchant: true,
+  lat:0,
+  lng:0)
 
 sightglass = Merchant.create(
   name: "Sightglass",
   email: "sightglass@sightglass.com",
   reward_name: "free coffee",
-  reward_threshold: 75),
-  lat:,
-  lng:
+  reward_threshold: 75,
+  active_merchant: true,
+  lat:0,
+  lng:0)
 
 tacko = Merchant.create(
   name: "Tacko",
   email: "tacko@tacko.com",
   reward_name: "free taco",
-  reward_threshold: 50),
-  lat:,
-  lng:
+  reward_threshold: 50,
+  active_merchant: true,
+  lat:0,
+  lng:0)
 
 monaghans = Merchant.create(
   name: "Monaghans",
   email: "monaghans@monaghans.com",
   reward_name: "free brewski",
-  reward_threshold: 150),
-  lat:,
-  lng:
+  reward_threshold: 150,
+  active_merchant: true,
+  lat:0,
+  lng:0)
 
-merchants = [bobs_donuts, cafe_brainwash, sightglass, tacko, monaghans]
+cheese_plus = Merchant.create(
+name: "Cheese Plus",
+email: "cheese@cheese.com",
+reward_name: "",
+reward_threshold: 60,
+active_merchant: false,
+lat:0,
+lng:0)
+
+marengo = Merchant.create(
+name: "Marengo",
+email: "marengo@marengo.com",
+reward_name: "",
+reward_threshold: 100,
+active_merchant: false,
+lat:0,
+lng:0)
+
+merchants = [bobs_donuts, cafe_brainwash, sightglass, tacko, monaghans, cheese_plus, marengo]
 
 
 consumers.each do |consumer|
@@ -92,9 +115,9 @@ consumers.each do |consumer|
       consumer_id: consumer.id,
       merchant_id: merchants[n].id,
       total: 0,
-      level: rand(1..10)),
+      level: rand(1..10),
       lat: merchants[n].lat,
-      lng: merchants[n].lng
+      lng: merchants[n].lng)
     n+=1
   end
 end
